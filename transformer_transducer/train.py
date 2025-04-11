24d4fb3a6168ad790f79bc7dfcd5e175bc0309b8
Nguyễn Anh Khoa <110364616+itsmekhoathekid@users.noreply.github.com>
2025-04-11 11:02:47 +0700

    Add files via upload

--- transformer_transducer/train.py
+++ transformer_transducer/train.py
@@ -2,7 +2,10 @@ import torch
 from utils.dataset import Speech2Text, speech_collate_fn
 from models.model import TransformerTransducer
 from tqdm import tqdm
-
+from models.loss import RNNTLoss
+import argparse
+import yaml
+import os 
 
 def train_one_epoch(model, dataloader, optimizer, criterion, device):
     model.train()
@@ -74,75 +77,62 @@ def evaluate(model, dataloader, criterion, device):
     print(f"✅ Average validation loss: {avg_loss:.4f}")
     return avg_loss
 
-class RNNTLoss(torch.nn.Module):
-    def __init__(self, blank=0, reduction="mean"):
-        super(RNNTLoss, self).__init__()
-        self.blank = blank
-        self.reduction = reduction
-
-    def forward(self, logits, targets, fbank_len, text_len):
-        # logits: [B, T, U, vocab_size]
-        # targets: [B, U]
-        # fbank_len: [B]
-        # text_len: [B]
-
-        # print(logits.shape)
-        # print(targets.shape)
-        # print(fbank_len.shape)
-        # print(text_len.shape)
-
-        loss = rnnt_loss(logits, targets[:, 1:].int(), fbank_len.int(), text_len.int() - 1, blank=self.blank)
-        
-        if self.reduction == "mean":
-            return loss.mean()
-        elif self.reduction == "sum":
-            return loss.sum()
-        else:
-            return loss
+def load_config(config_path):
+    with open(config_path, 'r') as f:
+        return yaml.safe_load(f)
         
 def main():
     from torch.optim import Adam
     
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, required=True, help="Path to YAML config file")
+    args = parser.parse_args()
+
+    config = load_config(args.config)
+    training_cfg = config['training']
+    optimizer_cfg = config['optimizer']
+
+
     # ==== Load Dataset ====
     train_dataset = Speech2Text(
-        json_path="/home/anhkhoa/transformer_transducer/data/train.json",
-        vocab_path="/home/anhkhoa/transformer_transducer/data/vocab.json"
+        json_path=training_cfg['train_path'],
+        vocab_path=training_cfg['vocab_path'],
     )
 
     train_loader = torch.utils.data.DataLoader(
         train_dataset,
-        batch_size=4,
+        batch_size= training_cfg['batch_size'],
         shuffle=True,
         collate_fn = speech_collate_fn
     )
 
     dev_dataset = Speech2Text(
-        json_path="/home/anhkhoa/transformer_transducer/data/dev.json",
-        vocab_path="/home/anhkhoa/transformer_transducer/data/vocab.json"
+        json_path=training_cfg['dev_path'],
+        vocab_path=training_cfg['vocab_path']
     )
 
     dev_loader = torch.utils.data.DataLoader(
         dev_dataset,
-        batch_size=4,
+        batch_size= training_cfg['batch_size'],
         shuffle=True,
         collate_fn = speech_collate_fn
     )
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = TransformerTransducer(
-        in_features=80,
+        in_features=config['model']['in_features'],
         n_classes=len(train_dataset.vocab),
-        n_layers=4,
-        n_dec_layers=2,
-        d_model=256,
-        ff_size=1024,
-        h=4,
-        joint_size=512,
-        enc_left_size=2,
-        enc_right_size=2,
-        dec_left_size=1,
-        dec_right_size=1,
-        p_dropout=0.1
+        n_layers=config['model']['n_layers'],
+        n_dec_layers=config['model']['n_dec_layers'],
+        d_model=config['model']['d_model'],
+        ff_size=config['model']['ff_size'],
+        h=config['model']['h'],
+        joint_size=config['model']['joint_size'],
+        enc_left_size=config['model']['enc_left_size'],
+        enc_right_size=config['model']['enc_right_size'],
+        dec_left_size=config['model']['dec_left_size'],
+        dec_right_size=config['model']['dec_right_size'],
+        p_dropout=config['model']['p_dropout']
     ).to(device)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -150,18 +140,32 @@ def main():
 
     # === Khởi tạo loss ===
     # Giả sử <blank> = 0, và bạn chưa dùng reduction 'mean' toàn bộ batch
-    criterion = RNNTLoss(blank = 0 , reduction="mean")  # hoặc "sum" nếu bạn custom average
+    criterion = RNNTLoss(config["rnnt_loss"]["blank"] , config["rnnt_loss"]["reduction"])  # hoặc "sum" nếu bạn custom average
 
     # === Optimizer ===
-    optimizer = Adam(model.parameters(), lr=1e-3)
+    optimizer = Adam(model.parameters(), lr=optimizer_cfg['lr'])
 
     # === Huấn luyện ===
-    num_epochs = 100
+    num_epochs = config["training"]["epochs"]
+
     for epoch in range(1, num_epochs + 1):
         train_loss = train_one_epoch(model, train_loader, optimizer, criterion, device)
         val_loss = evaluate(model,  dev_loader, criterion, device)
 
         print(f"📘 Epoch {epoch}: Train Loss = {train_loss:.4f}, Val Loss = {val_loss:.4f}")
+        # Save model checkpoint
+
+        model_filename = os.path.join(
+            config['training']['save_path'],
+            f"transformer_transducer_epoch_{epoch}"
+        )
+
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+        }, model_filename)
+
 
 if __name__ == "__main__":
     main()
