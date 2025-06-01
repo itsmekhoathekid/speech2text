c0c1cadeadbe2ec8110317473278f7603092093a
Nguyễn Anh Khoa <110364616+itsmekhoathekid@users.noreply.github.com>
2025-06-01 15:58:26 +0700

    Update construct.py

--- transformer_transducer/utils/construct.py
+++ transformer_transducer/utils/construct.py
@@ -27,7 +27,7 @@ def create_vocab(json_path):
 
     for idx, item in data.items():
         text = normalize_transcript(item['script'])
-        for word in text.strip():
+        for word in text.split(' '):
             if word not in vocab:
                 vocab[word] = len(vocab)
     
@@ -48,7 +48,7 @@ def process_data(data_path, vocab, default_data_path, save_path):
         data_res = {}
         text = normalize_transcript(item['script'])
         unk_id = vocab["<unk>"]
-        tokens = [vocab.get(word, unk_id) for word in text.strip()]
+        tokens = [vocab.get(word, unk_id) for word in text.strip().split()]
         data_res['encoded_text'] = tokens
         data_res['text'] = text
         data_res['wav_path'] = os.path.join(default_data_path, item['voice'])
@@ -58,16 +58,15 @@ def process_data(data_path, vocab, default_data_path, save_path):
     print(f"Data saved to {save_path}")
 
 
-vocab = create_vocab("/mnt/c/Users/VIET HOANG - VTS/Downloads/archive (2)/train.json")
-save_data(vocab, "/home/anhkhoa/transformer_transducer/data/vocab_char.json")
+vocab = create_vocab(r"/kaggle/input/vivos-dataset/train.json")
+save_data(vocab, r"/kaggle/working/vocab_w2i.json")
 
-process_data("/mnt/c/Users/VIET HOANG - VTS/Downloads/archive (2)/train.json",
+process_data(r"/kaggle/input/vivos-dataset/train.json",
              vocab,
-             "/mnt/d/voices/voices",
-             "/home/anhkhoa/transformer_transducer/data/train_char.json")
+             r"/kaggle/input/vivos-dataset/voices",
+             r"/kaggle/working/train_w2i.json")
 
-process_data("/mnt/c/Users/VIET HOANG - VTS/Downloads/archive (2)/test.json",
+process_data(r"/kaggle/input/vivos-dataset/test.json",
              vocab,
-             "/mnt/d/voices/voices",
-             "/home/anhkhoa/transformer_transducer/data/dev_char.json")
-
+             r"/kaggle/input/vivos-dataset/voices",
+             r"/kaggle/working/test_w2i.json")
